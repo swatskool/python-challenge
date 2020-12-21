2b82aba1ca69774a5fd0b65b35b32e85d15ad636
swatskool <75335665+swatskool@users.noreply.github.com>
2020-12-20 21:20:38 -0700

    final

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,37 +9,37 @@ change_sum = 0
 #my_dict = {'name':0}
 value = 0
 csv_path = os.path.join('..','PyBank', 'Homework_03-Python_PyBank_Resources_budget_data.csv')
-#csv_path = "..\PyBank\Homework_03-Python_PyBank_Resources_budget_data.csv"
-#csv_path = "Homework_03-Python_PyBank_Resources_budget_data"
 with open(csv_path, 'r') as f:
     csv_reader = csv.reader(f)
     header = next(csv_reader)#getting rid of header
     for line in csv_reader:
-        if int(line[1]):
             profit_loss.append(int(line[1])) # daily profit/loss
             total += int(line[1]) # total over the month
             dates.append(line[0]) # number of dates
 #f.close()
-
-for i in range(1,(len(profit_loss))):
-    value = profit_loss[i]-(profit_loss[i-1])
+end_value = len(profit_loss)
+ 
+for i in range(1,end_value):
+    value = profit_loss[i]-profit_loss[i-1]
     change.append(value) # change in profit loss
     change_sum += value # total profit/loss
+
 total_months = len(dates)
-average = change_sum/total_months
+average = change_sum/(total_months-1)
 max_change_month = dates[change.index(max(change))+1]
 min_change_month = dates[change.index(min(change))+1]
 max_change = max(change)
 min_change = min(change)
 
 #output on the terminal
+
 print('\nFinancial Analysis\n')
 print('-----------------------\n')
 print(f'Total Months: {total_months}\n')
 print(f'Total: $ {total}\n')
 print(f'Average  Change: $: {round(average,2)}\n')
 print(f'Greatest Increase in Profits: {max_change_month}  $({round(max_change)})\n')
-print(f'Greatest Increase in Profits: {min_change_month}  $({round(min_change)})\n')
+print(f'Greatest Decrease in Profits: {min_change_month}  $({round(min_change)})\n')
 
 #output in the new file named PyBank_Swati.txt
 with open('PyBank_Swati.txt', 'w') as f:
@@ -49,5 +49,5 @@ with open('PyBank_Swati.txt', 'w') as f:
     f.write(f'Total: $ {total}\n')
     f.write(f'Average  Change: $: {round(average,2)}\n')
     f.write(f'Greatest Increase in Profits: {max_change_month}  $({round(max_change)})\n')
-    f.write(f'Greatest Increase in Profits: {min_change_month}  $({round(min_change)})\n')
+    f.write(f'Greatest Decreasein Profits: {min_change_month}  $({round(min_change)})\n')
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 # initialising required libraries
 import os
 import csv
-import math
+from collections import OrderedDict
 county= {}
 name = {}
 # function that returns the key corresponding to the value in a dictionary
@@ -9,18 +9,6 @@ def get_key(my_dict,val):
     for key, value in my_dict.items():
          if val == value:
             return key
-# function to truncate decimals upto 3 places
-def truncate(number, decimals=0):
-
-    if not isinstance(decimals, int):
-        raise TypeError("decimal places must be an integer.")
-    elif decimals < 0:
-        raise ValueError("decimal places has to be 0 or more.")
-    elif decimals == 0:
-        return math.trunc(number)
-
-    factor = 10.0 ** decimals
-    return math.trunc(number * factor) / factor
  
 # opening the csv resource file
 csv_path = os.path.join('..','PyPoll', 'Homework_03-Python_PyPoll_Resources_election_data.csv')
@@ -62,7 +50,7 @@ print('--------------------------')
 print(f'Total Votes: {total}')
 print('--------------------------')
 
-
+count = OrderedDict(sorted(count.items(), reverse = True, key=lambda x: x[1])) # sorting the dictionary on descending value of votes
 for cname, votes in count.items(): 
    percent = float((votes/total)*100) # percentage votes per candidate
    print(f'{cname} : {round(percent)}% ({votes})')
